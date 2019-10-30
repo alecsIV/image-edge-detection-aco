7ceed9c3baaea5e4af54d74e917e9b811f03807a
Alexander Ivanov <alexanderiv.sci@gmail.com>
2019-10-30 23:49:00 +0000

    display image preview

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ const image = document.getElementById("image-output");
 
 uploader.addEventListener('change', function() {
     const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.addEventListener('load', function() {
+            console.log(this);
+            image.style.display = 'block';
+            image.setAttribute('src', this.result)
+        })
+        reader.readAsDataURL(file);
+    }
 });
