36bf3b39c65e30c526afc7ebde464f795933d210
Alexander Ivanov <alexanderiv.sci@gmail.com>
2019-10-30 23:24:50 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const uploader = document.getElementById('image-upload');
+const image = document.getElementById("image-output");
+
+uploader.addEventListener('change', function() {
+    const file = this.files[0];
+});
