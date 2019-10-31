07133d84267368951ec819dff176d926d7b3072f
Alexander Ivanov <alexanderiv.sci@gmail.com>
2019-10-31 00:09:41 +0000

    added canvas to transform image to matrix
    for some reason just zeros come out

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const uploader = document.getElementById('image-upload');
 const image = document.getElementById("image-output");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
 
 uploader.addEventListener('change', function() {
     const file = this.files[0];
@@ -8,8 +11,17 @@ uploader.addEventListener('change', function() {
         reader.addEventListener('load', function() {
             console.log(this);
             image.style.display = 'block';
-            image.setAttribute('src', this.result)
+            image.setAttribute('src', this.result);
+            drawMatrix(this.result);
         })
-        reader.readAsDataURL(file);
+        reader.readAsDataURL(file)
     }
 });
+
+function drawMatrix (source) {
+    const img = new Image();
+    img.src = img
+    ctx.drawImage(img, 0, 0);
+const imgData = ctx.getImageData(0, 0, 500, 500).data;
+console.log(imgData);
+}
