1c47fe427b537fb36cdb4f85bac4b0ebcc6e291e
Alexander Ivanov <alexanderiv.sci@gmail.com>
2019-10-31 15:34:02 +0000

    Fixed image preview sizes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const uploader = document.getElementById('image-upload');
-const image = document.getElementById("image-output");
+const image = document.getElementById("image-source");
+const imagePreview = document.getElementById("image-preview");
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
+const makeMatrixButton = document.getElementById('draw-matrix-button')
+
 
 
 uploader.addEventListener('change', function() {
@@ -9,19 +11,72 @@ uploader.addEventListener('change', function() {
     if (file) {
         const reader = new FileReader();
         reader.addEventListener('load', function() {
-            console.log(this);
-            image.style.display = 'block';
             image.setAttribute('src', this.result);
-            drawMatrix(this.result);
+            imagePreview.setAttribute('src', this.result);
         })
         reader.readAsDataURL(file)
     }
 });
 
-function drawMatrix (source) {
-    const img = new Image();
-    img.src = img
-    ctx.drawImage(img, 0, 0);
-const imgData = ctx.getImageData(0, 0, 500, 500).data;
-console.log(imgData);
+makeMatrixButton.addEventListener('click', function () {
+    console.log(image);
+    if (image) {
+        getMatrix(image);
+    }
+});
+function getMatrix(img) {
+    const ctx = canvas.getContext('2d');
+    drawImageProp(ctx, img, 0, 0, canvas.width, canvas.height);
+    // const imgData = ctx.getImageData(0, 0, img.width, img.height);
+    // console.log(imgData);
+}
+
+
+function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
+
+    if (arguments.length === 2) {
+        x = y = 0;
+        w = ctx.canvas.width;
+        h = ctx.canvas.height;
+    }
+
+    // default offset is center
+    offsetX = typeof offsetX === "number" ? offsetX : 0.5;
+    offsetY = typeof offsetY === "number" ? offsetY : 0.5;
+
+    // keep bounds [0.0, 1.0]
+    if (offsetX < 0) offsetX = 0;
+    if (offsetY < 0) offsetY = 0;
+    if (offsetX > 1) offsetX = 1;
+    if (offsetY > 1) offsetY = 1;
+
+    const iw = img.width,
+        ih = img.height,
+        r = Math.min(w / iw, h / ih)
+
+    let nw = iw * r,   // new prop. width
+        nh = ih * r,   // new prop. height
+        cx, cy, cw, ch, ar = 1;
+
+    // decide which gap to fill    
+    if (nw < w) ar = w / nw;                             
+    if (Math.abs(ar - 1) < 1e-14 && nh < h) ar = h / nh;  // updated
+    nw *= ar;
+    nh *= ar;
+
+    // calc source rectangle
+    cw = iw / (nw / w);
+    ch = ih / (nh / h);
+
+    cx = (iw - cw) * offsetX;
+    cy = (ih - ch) * offsetY;
+
+    // make sure source rectangle is valid
+    if (cx < 0) cx = 0;
+    if (cy < 0) cy = 0;
+    if (cw > iw) cw = iw;
+    if (ch > ih) ch = ih;
+
+    // fill image in dest. rectangle
+    ctx.drawImage(img, cx, cy, cw, ch,  x, y, w, h);
 }
