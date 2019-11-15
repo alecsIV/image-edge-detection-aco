bdb5273322f847af4cd23afb46682e3dcc87c16c
Alexander Ivanov <alexanderiv.sci@gmail.com>
2019-11-15 12:09:15 +0000

    animating movement
    not sure if the right way

--- src/components/aco/aco-algorithm.js
+++ src/components/aco/aco-algorithm.js
@@ -7,6 +7,9 @@ export default class ACO {
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
         this.matrixHelper = new MatrixHelper();
+        this.iterations = 25;
+        this.currentFrame = 0;
+
 
         // generate initial pheromone and heuristic matrices
         this.matrixHelper.generateInitialMatrices(image);
@@ -31,14 +34,16 @@ export default class ACO {
 
 
     drawAgent(agent) {
-        const xPrev = agent.previousCoordinates[agent.previousCoordinates.length - 1].x;
-        const yPrev = agent.previousCoordinates[agent.previousCoordinates.length - 1].y;
-        const x = agent.currentCoordinates.x;
-        const y = agent.currentCoordinates.y;
-        this.ctx.beginPath();
-        this.ctx.moveTo(xPrev, yPrev);
-        this.ctx.lineTo(x, y);
-        this.ctx.stroke();
+        this.ctx.fillRect(agent.currentCoordinates.x, agent.currentCoordinates.y);
+
+        // const xPrev = agent.previousCoordinates[agent.previousCoordinates.length - 1].x;
+        // const yPrev = agent.previousCoordinates[agent.previousCoordinates.length - 1].y;
+        // const x = agent.currentCoordinates.x;
+        // const y = agent.currentCoordinates.y;
+        // this.ctx.beginPath();
+        // this.ctx.moveTo(xPrev, yPrev);
+        // this.ctx.lineTo(x, y);
+        // this.ctx.stroke();
         // let dx = agent.currentCoordinates.x - agent.previousCoordinates[agent.previousCoordinates.length - 1].x;
         // let dy = agent.currentCoordinates.y - agent.previousCoordinates[agent.previousCoordinates.length - 1].y;
         // let maxD = (Math.max(Math.abs(dx), Math.abs(dy)) == Math.abs(dx)) ? Math.abs(dx) : Math.abs(dy);
@@ -61,29 +66,28 @@ export default class ACO {
     // }
 
     startSimulation() {
-        const iterations = 25;
-        for (let i = 0; i < iterations; i++) {
-            this.agents.forEach((agent, i) => {
-                const newCoordinates = agent.calculateNextStep();
-                agent.moveTo(newCoordinates);
-                if (agent.currentCoordinates == undefined) console.log('faulty', agent);
-                this.drawAgent(agent);
-            });
-        }
-        this.waitSeconds(500);
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.heigth);
         this.agents.forEach((agent, i) => {
-            if (i === 20) console.log('faulty', agent);
+            const newCoordinates = agent.calculateNextStep();
+            agent.moveTo(newCoordinates);
+            if (agent.currentCoordinates == undefined) console.log('faulty', agent);
             this.ctx.fillRect(agent.currentCoordinates.x, agent.currentCoordinates.y, agent.agentSize, agent.agentSize);
         });
-    }
-    waitSeconds(iMilliSeconds) {
-        var counter= 0
-            , start = new Date().getTime()
-            , end = 0;
-        while (counter < iMilliSeconds) {
-            end = new Date().getTime();
-            counter = end - start;
-        }
+
+        if (this.currentFrame !== this.iterations) {
+            window.requestAnimationFrame(this.startSimulation.bind(this));
+        } else console.log('END ANIMATION');
+        // this.agents.forEach((agent) => {
+        //     const foreachLoop = agent.previousCoordinates.forEach((prevPosition, i) => {
+        //         if (i < agent.previousCoordinates.length - 1) {
+        //             this.ctx.fillRect(prevPosition.x, agent.previousCoordinates[i+1].y, 1, 1);
+        //             window.requestAnimationFrame(foreachLoop);
+        //         } else {
+        //             this.ctx.fillRect(prevPosition.x, agent.currentCoordinates.y, 1, 1);
+        //             window.requestAnimationFrame(foreachLoop);
+        //         }
+        //     });
+
+        // });
     }
 }
